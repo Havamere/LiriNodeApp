9ae6f63b719ab54aee3093db1eaec547ff566640
Kyle R <Havamere@users.noreply.github.com>
2016-07-11 18:27:13 -0400

    deleted keys
    shouldn't have been uploaded

--- keys.js
+++
@@ -1,8 +0,0 @@
-console.log('this is loaded');
-
-exports.twitterKeys = {
-  consumer_key: '9tsiSbDBx4plwPjtwRWvt5lFY',
-  consumer_secret: 'v7ignCGPIGl3cXKZ5KWWtYwpA8plps9vaLYl9lqLqzmUiFhQKZ',
-  access_token_key: '1472329868-oswxl0RdoZpE6Za71lmJERJdASXUAblQQ1ZGk8J',
-  access_token_secret: 'S4ISHxzo0OM19NePpQa24ysQUdlzANkmT87qHegEjxBID',
-}
